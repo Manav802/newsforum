e50c9ab93319e0f4422eee5fc79f4a9d650e1584
Manav Chhabra <mchhabra@localhost.localdomain>
2021-06-24 19:44:16 +0530

    Updates

---
+++ precache-manifest.a141c757c03b01f01f13f4bcbc8be9b5.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff090df277f338d38df7208e5bbd0703",
+    "url": "/index.html"
+  },
+  {
+    "revision": "533e5a7e6c6dfd642e09",
+    "url": "/static/css/2.61f1384f.chunk.css"
+  },
+  {
+    "revision": "c2f51d8cffeaf789e7ef",
+    "url": "/static/css/main.ab3edd75.chunk.css"
+  },
+  {
+    "revision": "533e5a7e6c6dfd642e09",
+    "url": "/static/js/2.ae0bea36.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.ae0bea36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2f51d8cffeaf789e7ef",
+    "url": "/static/js/main.e4b5c113.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "c9f309b3d47969ecac64a77a6c672594",
+    "url": "/static/media/Nexa Bold.c9f309b3.otf"
+  },
+  {
+    "revision": "12108809f49c49bcdc126dcecc938761",
+    "url": "/static/media/Nexa Light.12108809.otf"
+  },
+  {
+    "revision": "d95152ab5a160491d28b3fce25bf4ff2",
+    "url": "/static/media/NunitoSans-Black.d95152ab.ttf"
+  },
+  {
+    "revision": "08e53a516d2ba719d98da46c49b3c369",
+    "url": "/static/media/NunitoSans-Bold.08e53a51.ttf"
+  },
+  {
+    "revision": "505a059580cfbeaccdcb7a489bb67ec9",
+    "url": "/static/media/NunitoSans-ExtraBold.505a0595.ttf"
+  },
+  {
+    "revision": "4c8f447011eef80831b45edb1e5971e0",
+    "url": "/static/media/NunitoSans-Regular.4c8f4470.ttf"
+  },
+  {
+    "revision": "bd318b58018198a57723f311627492ac",
+    "url": "/static/media/NunitoSans-SemiBold.bd318b58.ttf"
+  }
+]);
